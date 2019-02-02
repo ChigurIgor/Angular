872d572e9f2715c044e16f32f054600af01ae376
IGOR CHIGUR <chigur.ig@gmail.com>
2019-02-02 23:32:31 +0200

    sdfgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <h1>My text</h1>
+  //     <img src="./favicon.ico">`,
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'Angular2';
+  title = 'My New Title';
 }
