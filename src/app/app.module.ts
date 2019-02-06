83cd80529fa198e8bd36a458d6648a03b59108dd
IGOR CHIGUR <chigur.ig@gmail.com>
2019-02-06 15:44:50 +0200

    working with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MynewcomponentComponent } from './mynewcomponent/mynewcomponent.component';
@@ -10,6 +11,15 @@ import { AboutComponent } from './about/about.component';
 import { NewsComponent } from './news/news.component';
 import { PriceComponent } from './price/price.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'news', component : NewsComponent},
+  {path : 'price', component : PriceComponent},
+  {path : '**', component : NotFoundComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -24,7 +34,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
