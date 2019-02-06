76737b82a118dcac973bdfff5d9da11286f8c91e
IGOR CHIGUR <chigur.ig@gmail.com>
2019-02-06 15:36:59 +0200

    working with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppComponent } from './app.component';
 import { MynewcomponentComponent } from './mynewcomponent/mynewcomponent.component';
 import { DateComponent } from './date/date.component';
 import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { PriceComponent } from './price/price.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MynewcomponentComponent,
-    DateComponent
+    DateComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    PriceComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
